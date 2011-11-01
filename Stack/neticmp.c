6a0fb3d82a70606f4e48d6719a65fa9e47a2cb1a
first-developer <lioninho11@hotmail.fr>
2011-11-01 03:47:06 +0100

    - added : definition of the function icmpSendPacket and code related to

--- Stack/neticmp.c
+++ Stack/neticmp.c
@@ -71,38 +71,68 @@ unsigned char icmpSendPacket(EventsEvent *event,EventsSelector *selector){
 
   // Get icmp data from the selector
   AssocArray *infos=(AssocArray *)selector->data_this;
-/*
+  
   // Checking presence of ICMP attributes
-  if( arraysTestIndex(infos,"type",0)<0 || 
-      arraysTestIndex(infos,"code",0)<0)||
-      arraysTestIndex(infos,"size",0)<0)||
-      arraysTestIndex(infos,"data",0)<0) )
+  if( arraysTestIndex(infos,"type",0)<0 || arraysTestIndex(infos,"code",0)<0 ||
+      arraysTestIndex(infos,"size",0)<0 || arraysTestIndex(infos,"data",0)<0 ||
+      arraysTestIndex(infos,"ldst",0)<0 )
   { arraysFreeArray(infos); return 1; } 
 	
-  // Get ICMP attributes: type, code, data
-  unsigned char *type=(unsigned char *)arraysGetValue(infos,"type",NULL,0); //type
-  unsigned char *code=(unsigned char *)arraysGetValue(infos,"code",NULL,0); //code
-  unsigned char *size=(unsigned char *)arraysGetValue(infos,"size",NULL,0); //size
-  unsigned char *data=(unsigned char *)arraysGetValue(infos,"data",NULL,0); //data
+	// Get the ip layer
+	StackLayers *pip=stackFindProtoById(LEVEL_NETWORK,ETHERNET_PROTO_IP);
+	if(pip==NULL || pip->event_out<0){ arraysFreeArray(infos); return 0; }
 
-  // Compute checksum
-  unsigned short int checksum=genericChecksum(data,size);
+  // Get ICMP attributes: type, code, data, ldst
+  unsigned char type	=	*(unsigned char *)arraysGetValue(infos,"type",NULL,0); //type
+  unsigned char code	=	*(unsigned char *)arraysGetValue(infos,"code",NULL,0); //code
+  int 			data_size	=	*(int *)arraysGetValue(infos,"size",NULL,0); //size
+  unsigned char *data	=	(unsigned char *)arraysGetValue(infos,"data",NULL,0); //data
+	IPv4_fields 		*ip	=	(IPv4_fields *)arraysGetValue(infos,"ldst", NULL, 0); 	//ldst
+	IPv4Address	 source = (IPv4Address) ip->source;  // src adresse related to the error
+	IPv4Address	 target = IPV4_ADDRESS_NULL;  // set target adresse to NULL address
+	
+	// free infos datas
+	arraysFreeArray(infos);
 
-  // et icmp header size
-  int icmp_header_size = 4;
+	// Fill ICMP Header 
+	// -----------------------------------------------------------------------
+  // Compute size_hicmp and size_icmp
+	int size_hicmp = sizeof(ICMPv4_fields)-1;
+	int size_icmp  = data_size + size_hicmp;
 
-  // Init and set icmp header 
-  data=(unsigned char *)realloc(data, size+icmp_header_size);
-  memmove(data+4, data, size);
-  bzero(data,4);
+	// Reallocate data space memory and check if it's done well
+  data=(unsigned char *) realloc(data, size_icmp);
+	if (data == NULL) { perror("icmpSendPacket.realloc"); return 1; }
+	
+	// put icmp_ header before icmp data
+	memmove(data+size_hicmp,data,data_size);
+	bzero(data,size_hicmp);  // fill icmp header with zero
 
-  arraysSetValue(&icmp_infos,"type",&type,sizeof(unsigned char),0);
-  arraysSetValue(&icmp_infos,"code",&code,sizeof(unsigned char),0);
-  arraysSetValue(&icmp_infos,"checksum",data,reply_size,AARRAY_DONT_DUPLICATE);
-  arraysSetValue(&icmp_infos,"size",&reply_size,sizeof(int),0);
-  arraysSetValue(&icmp_infos,"ldst",&source,sizeof(IPv4Address),0);
-  eventsTrigger(picmp->event_out,icmp_infos);  
-*/
+	// Apply the ICMP structure to the data by casting data
+	ICMPv4_fields *icmp = (ICMPv4_fields *)data;
 
+	// Set attributes of icmp structure
+	icmp->type 	= type;
+	icmp->code	= code;
+	// Compute and set checksum attribute
+  unsigned short int checksum=genericChecksum(data,size_icmp);
+	icmp = (ICMPv4_fields *)data; 
+	icmp->checksum = checksum;
+
+	/* TODO: verbose for debugging icmpi */
+
+	// Call IP layer
+	unsigned char protocol = IPV4_PROTOCOL_UDP;
+	AssocArray *ip_options = NULL;
+	arraysSetValue(&ip_options,"lsrc",&source,sizeof(IPv4Address),0);
+	//Set ip infos
+	AssocArray *ip_infos=NULL;
+	arraysSetValue(&ip_infos,"ldst",&target,sizeof(IPv4Address),0);
+	arraysSetValue(&ip_infos,"proto",&protocol,sizeof(unsigned char),0);
+	arraysSetValue(&ip_infos,"data",data,size_icmp,AARRAY_DONT_DUPLICATE);
+	arraysSetValue(&ip_infos,"size",&size_icmp,sizeof(int),0);
+	arraysSetValue(&ip_infos,"opts",ip_options,sizeof(AssocArray *), AARRAY_DONT_DUPLICATE);
+	eventsTrigger(pip->event_out,ip_infos);
+	
 	return 0;
 }
